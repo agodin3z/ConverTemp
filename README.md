800f951ca1c9c26a93348a1620f658c815405cfe
AGodinez <pistilloagodinez@gmail.com>
2014-09-26 21:35:40 -0600

    Agregue codigo de la app

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 TempConverter
 =============
 
-Conversor de unidades de temperatura para FirefoxOS
+![TempConverter](./images/icon.png)
+
+Conversor de unidades de temperatura para FirefoxOS, **Version 0.1**
+
+## Lenguaje y Librerías ##
+* HTML5
+* JQuery Mobile
+* JavaScript
+
+## Desarrollador ##
+* Andrés Godínez
+
+## Licencia ##
+* Mozilla Public License 2.0
---
+++ js/calcular.js
@@ -0,0 +1,24 @@
+function convertir(){
+  var grados=document.getElementById('grados').value,
+      lon = document.getElementById('grados').value.length;
+  
+  grados=parseFloat(grados);
+  if(lon > 0) {
+    if($("input[value=celsius]:checked").val()) {
+      cf = grados + " °C = " + parseFloat((1.8*grados)+32).toFixed(2) + " °F";
+      ck = grados + " °C = " + parseFloat(grados + 273).toFixed(2) + " K";
+      alert(cf + "\n\n" + ck);
+    } else if($("input[value=farenheith]:checked").val()) {
+      fc = grados + " °F = " + parseFloat((grados-32)/1.8).toFixed(2) + " °C";
+      fk = grados + " °F = " + parseFloat(((grados-32)/1.8)+273).toFixed(2) + " K";
+      alert(fc + "\n\n" + fk);
+    } else {
+      kc = grados + " K = " + parseFloat(grados - 273).toFixed(2) + " °C";
+      kf = grados + " K = " + parseFloat(((grados-273)*1.8)+32).toFixed(2) + " °F";
+      alert(kc + "\n\n" + kf);
+    }
+  } else {
+    alert("No ha ingresado ningun valor");
+  }
+};
+
